e14a1ef405f5ec1abac4f0b920fddabf2c76ae2a
Murray Grant <bitbucket@ligos.net>
2018-04-12 21:08:57 +1000

    Refactor the main entropy loop in pooled generator - logging, pull into methods, improve first seed generation.

--- Terninger/Generator/PooledEntropyCprngGenerator.cs
+++ Terninger/Generator/PooledEntropyCprngGenerator.cs
@@ -10,6 +10,7 @@ using System.Security.Cryptography;
 using MurrayGrant.Terninger.Helpers;
 using MurrayGrant.Terninger.Accumulator;
 using MurrayGrant.Terninger.EntropySources;
+using MurrayGrant.Terninger.LibLog;
 
 using BigMath;
 
@@ -35,6 +36,7 @@ namespace MurrayGrant.Terninger.Generator
         private readonly Thread _SchedulerThread;
 
         private bool _Disposed = false;
+        private readonly static ILog Logger = LogProvider.For<PooledEntropyCprngGenerator>();
 
         public int MaxRequestBytes => _Prng.MaxRequestBytes;
  
@@ -55,11 +57,17 @@ namespace MurrayGrant.Terninger.Generator
         private readonly CancellationTokenSource _ShouldStop = new CancellationTokenSource();
         private readonly EventWaitHandle _WakeSignal = new EventWaitHandle(false, EventResetMode.AutoReset);
         private readonly WaitHandle[] _AllSignals;
+        private const int WakeReason_ExternalReseed = 0;
+        private const int WakeReason_GeneratorStopped = 1;
+        private const int WakeReason_SleepElapsed = WaitHandle.WaitTimeout;
 
         /// <summary>
         /// Event is raised after each time the generator is reseeded.
         /// </summary>
         public event EventHandler<PooledEntropyCprngGenerator> OnReseed;
+
+        // TODO: make this configurable.
+        private const int MinEntropyForReseed = 48;
 
 
         public PooledEntropyCprngGenerator(IEnumerable<IEntropySource> initialisedSources) 
@@ -87,6 +95,7 @@ namespace MurrayGrant.Terninger.Generator
             _SchedulerThread.Name = "Terninger Worker Thread - " + UniqueId.ToString("X");
             _SchedulerThread.IsBackground = true;
             this.EntropyPriority = EntropyPriority.High;        // A new generator must reseed as quickly as possible.
+            // Important, the index of these are used in WakeReason()
             _AllSignals = new[] { _WakeSignal, _ShouldStop.Token.WaitHandle };
         }
 
@@ -107,8 +116,15 @@ namespace MurrayGrant.Terninger.Generator
             }
         }
 
-        // Methods to get random bytes, as part of IRandomNumberGenerator.
+        /// <summary>
+        /// Fill the supplied buffer with random bytes.
+        /// This will throw if the inital seed has not been generated - await StartAndWaitForFirstSeed() first.
+        /// </summary>
         public void FillWithRandomBytes(byte[] toFill) => FillWithRandomBytes(toFill, 0, toFill.Length);
+        /// <summary>
+        /// File the supplied buffer with count random bytes at the specified offset.
+        /// This will throw if the inital seed has not been generated - await StartAndWaitForFirstSeed() first.
+        /// </summary>
         public void FillWithRandomBytes(byte[] toFill, int offset, int count)
         {
             if (this.ReseedCount == 0)
@@ -119,17 +135,29 @@ namespace MurrayGrant.Terninger.Generator
                 _Prng.FillWithRandomBytes(toFill, offset, count);
             }
             this.BytesRequested = this.BytesRequested + count;
+            // Any request for data in low priority should raise the level to normal.
+            if (this.EntropyPriority == EntropyPriority.Low)
+                this.EntropyPriority = EntropyPriority.Normal;
         }
 
 
 
 
+        /// <summary>
+        /// Start the main entropy loop, to gather entropy over time.
+        /// </summary>
         public void Start()
         {
             this._SchedulerThread.Start();
         }
 
+        /// <summary>
+        /// Start the main entropy loop, and wait for the first seed.
+        /// </summary>
         public Task StartAndWaitForFirstSeed() => StartAndWaitForNthSeed(1);
+        /// <summary>
+        /// Start the main entropy loop, and wait for the specified reseed event.
+        /// </summary>
         public Task StartAndWaitForNthSeed(Int128 seedNumber)
         {
             if (this.ReseedCount >= seedNumber)
@@ -138,6 +166,9 @@ namespace MurrayGrant.Terninger.Generator
             return WaitForNthSeed(seedNumber);
         }
 
+        /// <summary>
+        /// Wait for the specified reseed event.
+        /// </summary>
         private async Task WaitForNthSeed(Int128 seedNumber)
         {
             // TODO: work out how to do this without polling.
@@ -145,12 +176,20 @@ namespace MurrayGrant.Terninger.Generator
                 await Task.Delay(100);
         }
 
+        /// <summary>
+        /// Stop the generater from gathering entropy.
+        /// </summary>
         public void RequestStop()
         {
+            Logger.Debug("Sending stop signal to generator thread.");
             _ShouldStop.Cancel();
         }
+        /// <summary>
+        /// Stop the generater from gathering entropy. A Task is returned when the generator has stopped.
+        /// </summary>
         public async Task Stop()
         {
+            Logger.Debug("Sending stop signal to generator thread.");
             _ShouldStop.Cancel();
             await Task.Delay(1);
             // TODO: work out how to do this without polling. Thread.Join() perhaps.
@@ -158,16 +197,37 @@ namespace MurrayGrant.Terninger.Generator
                 await Task.Delay(100);
         }
 
+        /// <summary>
+        /// Request the internal random number generator be reseeded as soon as possible.
+        /// </summary>
         public void StartReseed()
         {
             Reseed();
         }
+        /// <summary>
+        /// Request the internal random number generator be reseeded as soon as possible. A Task is returned when the reseed is completed.
+        /// </summary>
         public Task Reseed() {
+            Logger.Debug("Received external reseed request.");
             this.EntropyPriority = EntropyPriority.High;
             this._WakeSignal.Set();
             return WaitForNthSeed(this.ReseedCount + 1);
         }
 
+        /// <summary>
+        /// Resets the entropy contained in the first (zero) pool.
+        /// When combined with Reseed(), this forces a minimal amount of entropy to be gathered before a reseed.
+        /// This can be useful after reloading the generator from disk, or resuming from hybernation / sleep.
+        /// </summary>
+        public void ResetPoolZero()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// Add an initialised and ready to use entropy source to the generator.
+        /// </summary>
         public void AddInitialisedSource(IEntropySource source)
         {
             lock(_EntropySources)
@@ -179,81 +239,170 @@ namespace MurrayGrant.Terninger.Generator
 
         private void ThreadLoop()
         {
+            try
+            {
+                ThreadLoopInner();
+            }
+            catch (Exception ex)
+            {
+                Logger.FatalException("Unhandled exception in generator. Generator will now stop, no further entropy will be generated or available.", ex);
+                this.Dispose();
+            }
+        }
+        private void ThreadLoopInner()
+        {
             while (!_ShouldStop.IsCancellationRequested)
             {
-                // Read and randomise sources.
-                IEntropySource[] sources;
-                lock(_EntropySources)
+                Logger.Trace("Running entropy loop.");
+
+                var sources = GetSources();
+                if (sources == null)
                 {
-                    sources = _EntropySources.ToArray();
-                }
-                // There may not be any sources until some time after the generator is started.
-                if (sources.Length == 0)
-                {
-                    // The thread should be woken on cancellation or external signal.
-                    int wakeIdx = WaitHandle.WaitAny(_AllSignals, 100);
-                    var wasTimeout = wakeIdx == WaitHandle.WaitTimeout;
+                    Logger.Trace("No entropy sources available yet.");
                     continue;
                 }
-                lock (_PrngLock)
-                {
-                    // Sources are shuffled so the last source isn't easily determined (the last source can bias the accumulator, particularly if malicious).
-                    sources.ShuffleInPlace(_Prng);
-                }
+
 
                 // Poll all sources.
-                // TODO: read up to N in parallel.
-                foreach (var source in _EntropySources)
-                {
-                    byte[] maybeEntropy;
-                    try
-                    {
-                        // These may come from 3rd parties, use external hardware or do IO: anything could go wrong!
-                        maybeEntropy = source.GetEntropyAsync(this.EntropyPriority).GetAwaiter().GetResult();
-                    }
-                    catch (Exception ex)
-                    {
-                        // TODO: log exception
-                        // TODO: if a particular source keeps throwing, should we ignore it??
-                        maybeEntropy = null;
-                    }
-                    if (maybeEntropy != null)
-                    {
-                        lock (_AccumulatorLock)
-                        {
-                            _Accumulator.Add(new EntropyEvent(maybeEntropy, source));
-                        }
-                    }
-                    if (_ShouldStop.IsCancellationRequested)
-                        break;
-                }
+                Logger.Trace("Gathering entropy from {0:N0} source(s).", sources.Count());
+                this.PollSources(sources);
+                Logger.Trace("Accumulator stats (bytes): available entropy = {0}, first pool entropy = {1}, min pool entropy = {2}, max pool entropy = {3}, total entropy ever seen {4}.", _Accumulator.AvailableEntropyBytesSinceLastSeed, _Accumulator.PoolZeroEntropyBytesSinceLastSeed, _Accumulator.MinPoolEntropyBytesSinceLastSeed, _Accumulator.MaxPoolEntropyBytesSinceLastSeed, _Accumulator.TotalEntropyBytes);
 
-                // Determine if we should re-seed.
-                if (this.ShouldReseed())
-                {
-                    byte[] seedMaterial;
-                    lock (_AccumulatorLock)
-                    {
-                        seedMaterial = _Accumulator.NextSeed();
-                    }
-                    lock (this._PrngLock)
-                    {
-                        this._Prng.Reseed(seedMaterial);
-                    }
-                    if (this.EntropyPriority == EntropyPriority.High)
-                        this.EntropyPriority = EntropyPriority.Normal;
-                    this.OnReseed?.Invoke(this, this);
-                }
+                
+                // Reseed the generator (if requirements for reseed are met).
+                bool didReseed = MaybeReseedGenerator();
+
+                // Update the priority based on recent data requests.
+                MaybeUpdatePriority(didReseed);
 
                 // Wait for some period of time before polling again.
                 var sleepTime = WaitTimeBetweenPolls();
+                Logger.Trace("Sleeping entropy loop for {0}.", sleepTime);
                 if (sleepTime > TimeSpan.Zero)
                 {
                     // The thread should be woken on cancellation or external signal.
                     int wakeIdx = WaitHandle.WaitAny(_AllSignals, sleepTime);
-                    var wasTimeout = wakeIdx == WaitHandle.WaitTimeout;
+                    Logger.Trace("Entropy loop woken up, reason: {0}", WakeReason(wakeIdx));
                 }
             }
+        }
+
+        private IEnumerable<IEntropySource> GetSources()
+        {
+            // Read and randomise sources.
+            IEntropySource[] sources;
+            lock (_EntropySources)
+            {
+                sources = _EntropySources.ToArray();
+            }
+            // There may not be any sources until some time after the generator is started.
+            if (sources.Length == 0)
+            {
+                // The thread should be woken on cancellation or external signal.
+                int wakeIdx = WaitHandle.WaitAny(_AllSignals, 100);
+                var wasTimeout = wakeIdx == WaitHandle.WaitTimeout;
+                return null;
+            }
+
+            lock (_PrngLock)
+            {
+                // Sources are shuffled so the last source isn't easily determined (the last source can bias the accumulator, particularly if malicious).
+                sources.ShuffleInPlace(_Prng);
+            }
+            return sources;
+        }
+
+        private void PollSources(IEnumerable<IEntropySource> sources)
+        {
+            // Poll all sources.
+            // TODO: read up to N in parallel.
+            foreach (var source in sources)
+            {
+                byte[] maybeEntropy;
+                try
+                {
+                    // These may come from 3rd parties, use external hardware or do IO: anything could go wrong!
+                    Logger.Trace("Reading entropy from source '{0}' (of type '{1}'.", source.Name, source.GetType().Name);
+                    maybeEntropy = source.GetEntropyAsync(this.EntropyPriority).GetAwaiter().GetResult();
+                    if (maybeEntropy == null || maybeEntropy.Length == 0)
+                        Logger.Trace("Read {0:N0} byte(s) of entropy from source '{1}' (of type '{2}').", 0, source.Name, source.GetType().Name);
+                    else
+                        Logger.Debug("Read {0:N0} byte(s) of entropy from source '{1}' (of type '{2}').", maybeEntropy.Length, source.Name, source.GetType().Name);
+                }
+                catch (Exception ex)
+                {
+                    Logger.ErrorException("Unhandled exception from entropy source '{0}' (of type '{1}').", ex, source.Name, source.GetType().Name);
+                    // TODO: if a particular source keeps throwing, should we ignore it??
+                    maybeEntropy = null;
+                }
+
+
+                if (maybeEntropy != null)
+                {
+                    lock (_AccumulatorLock)
+                    {
+                        _Accumulator.Add(new EntropyEvent(maybeEntropy, source));
+                    }
+                }
+                if (_ShouldStop.IsCancellationRequested)
+                    break;
+                if (this.EntropyPriority == EntropyPriority.High && this.ShouldReseed())
+                {
+                    // Break out of the loop early when in high priority 
+                    Logger.Trace("Have gathered a minimal amount of entropy in High priority, ending entropy source loop early.");
+                    break;
+                }
+            }
+        }
+
+        private bool MaybeReseedGenerator()
+        {
+            var didReseed = false;
+
+            if (this.ShouldReseed())
+            {
+                Logger.Debug("Beginning re-seed. Accumulator stats (bytes): available entropy = {0}, first pool entropy = {1}, min pool entropy = {2}, max pool entropy = {3}, total entropy ever seen {4}.", _Accumulator.AvailableEntropyBytesSinceLastSeed, _Accumulator.PoolZeroEntropyBytesSinceLastSeed, _Accumulator.MinPoolEntropyBytesSinceLastSeed, _Accumulator.MaxPoolEntropyBytesSinceLastSeed, _Accumulator.TotalEntropyBytes);
+                byte[] seedMaterial;
+                lock (_AccumulatorLock)
+                {
+                    seedMaterial = _Accumulator.NextSeed();
+                }
+                Logger.Trace("Got {0:N0} bytes of entropy from {1:N0} accumulator pool(s) as seed material.", seedMaterial.Length, _Accumulator.PoolCountUsedInLastSeedGeneration);
+
+                lock (this._PrngLock)
+                {
+                    this._Prng.Reseed(seedMaterial);
+                }
+                didReseed = true;
+                Logger.Trace("Reseed complete.");
+
+
+
+                Logger.Trace("Raising OnReseed event.");
+                try
+                {
+                    // 3rd party code paranoia.
+                    this.OnReseed?.Invoke(this, this);
+                }
+                catch (Exception ex)
+                {
+                    Logger.WarnException("Exception when raising OnReseed event.", ex);
+                }
+            }
+            return didReseed;
+        }
+
+        private void MaybeUpdatePriority(bool didReseed)
+        {
+            if (didReseed && this.EntropyPriority == EntropyPriority.High)
+            {
+                // If we reseed in high priorty, drop to normal priority.
+                this.EntropyPriority = EntropyPriority.Normal;
+                Logger.Debug("After reseed in High priority, dropping to normal.");
+            }
+            //else if (!didReseed && this.EntropyPriority == EntropyPriority.Normal && this.LastRequest > some long time)
+                // If there has been a long time since the last request, drop to low priority.
+                //this.EntropyPriority = EntropyPriority.Low;
         }
 
         private bool ShouldReseed()
@@ -263,7 +412,7 @@ namespace MurrayGrant.Terninger.Generator
                 return false;
             else if (this.EntropyPriority == EntropyPriority.High)
                 // TODO: configure how much entropy we need to accumulate before reseed.
-                return this._Accumulator.PoolZeroEntropyBytesSinceLastSeed > 48;
+                return this._Accumulator.PoolZeroEntropyBytesSinceLastSeed > MinEntropyForReseed;
             else if (this.EntropyPriority == EntropyPriority.Low)
                 // TODO: use priority, rate of consumption and date / time to determine when to reseed.
                 return this._Accumulator.MinPoolEntropyBytesSinceLastSeed > 256;
@@ -278,5 +427,10 @@ namespace MurrayGrant.Terninger.Generator
                                                  : EntropyPriority == EntropyPriority.Normal ? TimeSpan.FromSeconds(5)
                                                  : EntropyPriority == EntropyPriority.Low ? TimeSpan.FromSeconds(30)
                                                  : TimeSpan.FromSeconds(1);     // Impossible case.
+
+        private string WakeReason(int wakeIdx) => wakeIdx == WakeReason_SleepElapsed ? "sleep time elapsed"
+                                                : wakeIdx == WakeReason_ExternalReseed ? "external reseed request"
+                                                : wakeIdx == WakeReason_GeneratorStopped ? "generator stopped"
+                                                : "unknown";
     }
 }
