51d3e61a1dea5b9437e281c5c12efe41e8d2daca
Murray Grant <github@ligos.net>
2019-11-27 18:31:30 +1100

    Add missing files :-/

---
+++ Terninger.Random.Pooled/EntropySources/AsyncHintAttribute.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MurrayGrant.Terninger.EntropySources
+{
+    /// <summary>
+    /// Marks how often an IEntropySource is expected to actually be async.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class AsyncHintAttribute : Attribute
+    {
+        public readonly IsAsync IsAsync;
+
+        public AsyncHintAttribute(IsAsync isAsync)
+        {
+            this.IsAsync = isAsync;
+        }
+    }
+
+    public enum IsAsync
+    {
+        /// <summary>
+        /// Unknown how often GetEntroptyAsync() is async.
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// GetEntroptyAsync() is never async; it will always resolve synchronously.
+        /// </summary>
+        Never = 1,
+
+        /// <summary>
+        /// GetEntroptyAsync() is async on first call; afterwards it will always resolve synchronously.
+        /// </summary>
+        AfterInit = 2,
+
+        /// <summary>
+        /// GetEntroptyAsync() is occasionally async (under 10% of the time); it will usually resolve synchronously.
+        /// </summary>
+        Rarely = 3,
+
+        /// <summary>
+        /// GetEntroptyAsync() is mostly async (over 10% of the time); it will occasionally resolve synchronously.
+        /// </summary>
+        Mostly = 4,
+
+        /// <summary>
+        /// GetEntroptyAsync() is always async; it will never resolve synchronously.
+        /// </summary>
+        Always = 5,
+    }
+}
---
+++ Terninger.Random.Pooled/Random/SourceAndMetadata.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MurrayGrant.Terninger.EntropySources;
+
+namespace MurrayGrant.Terninger.Random
+{
+    internal class SourceAndMetadata
+    {
+        public SourceAndMetadata(IEntropySource source, string uniqueName)
+        {
+            Source = source ?? throw new ArgumentNullException(nameof(source));
+            UniqueName = uniqueName ?? throw new ArgumentNullException(nameof(uniqueName));
+#if NETSTANDARD1_3
+            AsyncHint = IsAsync.Unknown;
+#else
+            var maybeHintAttribute = source.GetType().GetCustomAttributesData().FirstOrDefault(x => x.AttributeType == typeof(AsyncHintAttribute));
+            if (maybeHintAttribute != null)
+            {
+                AsyncHint = (IsAsync)maybeHintAttribute.ConstructorArguments.First(x => x.ArgumentType == typeof(IsAsync)).Value;
+            }
+#endif
+            if (AsyncHint == IsAsync.AfterInit)
+                AsyncScore = -8;
+            else if (AsyncHint == IsAsync.Never)
+                AsyncScore = -10;
+            else if (AsyncHint == IsAsync.Rarely)
+                AsyncScore = 5;
+            else
+                AsyncScore = 10;
+        }
+
+        public string UniqueName { get; }
+        public IEntropySource Source { get; }
+        public IsAsync AsyncHint { get; }
+
+        /// <summary>
+        /// Positive is biased toward async, negative is biased toward sync.
+        /// </summary>
+        public int AsyncScore { get; private set; }
+        /// <summary>
+        /// Positive is biased toward success, negative is biased toward exceptions.
+        /// </summary>
+        public int ExceptionScore { get; private set; }
+
+        public override string ToString() => $"{UniqueName} - {AsyncHint}. AsyncScore: {AsyncScore}, ExceptionScore: {ExceptionScore}";
+
+        public void ScoreAsync()
+        {
+            if (AsyncScore <= 1000)
+                AsyncScore = AsyncScore + 2;
+        }
+        public void ScoreSync()
+        {
+            if (AsyncScore >= -1000)
+                AsyncScore = AsyncScore - 1;
+        }
+
+        public void ScoreException()
+        {
+            if (ExceptionScore >= -1000)
+                ExceptionScore = ExceptionScore - 5;
+        }
+        public void ScoreSuccess()
+        {
+            if (ExceptionScore <= 1000)
+                ExceptionScore = ExceptionScore + 1;
+        }
+    }
+}
