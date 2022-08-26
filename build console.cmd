4d0074f02205fdc7ac4838f71c2a3f434b20e44a
Murray Grant <github@ligos.net>
2022-08-26 20:33:43 +1000

    Version bumps and fixes for building console app

--- build console.cmd
+++ build console.cmd
@@ -2,35 +2,38 @@ rem Rebuild
 dotnet clean -c Release
 dotnet build -c Release
 
-rem Build ZIP file for .NET 4.5.2
-del /q Terninger.Console.net452.zip
-cd Terninger.Console\bin\Release\net452
-"c:\Program Files\7-Zip\7z.exe" a -mx9 ..\..\..\..\Terninger.Console.net452.zip *
+rem Build ZIP file for .NET 4.8
+del /q Terninger.Console.net48.zip
+cd Terninger.Console\bin\Release\net48
+"c:\Program Files\7-Zip\7z.exe" a -mx9 ..\..\..\..\Terninger.Console.net48.zip *
 cd ..\..\..\..
-"c:\Program Files\7-Zip\7z.exe" u Terninger.Console.net452.zip LICENSE.txt
+"c:\Program Files\7-Zip\7z.exe" u Terninger.Console.net48.zip LICENSE.txt
 
 
-rem Publish for .NET Core 2.1
-dotnet publish Terninger.Console -c Release -f netcoreapp2.1 
+rem Publish for .NET Core 3.1
+dotnet publish Terninger.Console -c Release -f netcoreapp3.1 
 
-rem Build ZIP file for .NET Core 2.1
-del /q Terninger.Console.netcoreapp21.zip
-cd Terninger.Console\bin\Release\netcoreapp2.1\publish
-"c:\Program Files\7-Zip\7z.exe" a -mx9 ..\..\..\..\..\Terninger.Console.netcoreapp21.zip *
+rem Build ZIP file for .NET Core 3.1
+del /q Terninger.Console.netcoreapp31.zip
+cd Terninger.Console\bin\Release\netcoreapp3.1\publish
+"c:\Program Files\7-Zip\7z.exe" a -mx9 ..\..\..\..\..\Terninger.Console.netcoreapp31.zip *
 cd ..\..\..\..\..
-"c:\Program Files\7-Zip\7z.exe" u Terninger.Console.netcoreapp21.zip LICENSE.txt
+"c:\Program Files\7-Zip\7z.exe" u Terninger.Console.netcoreapp31.zip LICENSE.txt
+"c:\Program Files\7-Zip\7z.exe" d Terninger.Console.netcoreapp31.zip Terninger.Console.exe.config
+"c:\Program Files\7-Zip\7z.exe" d Terninger.Console.netcoreapp31.zip Terninger.Console.dll.config
 
 
-rem Publish for .NET Core 3.0
-dotnet publish Terninger.Console -c Release -f netcoreapp3.0
+rem Publish for .NET 6.0
+dotnet publish Terninger.Console -c Release -f net60
 
-rem Build ZIP file for .NET Core 3.0
-del /q Terninger.Console.netcoreapp30.zip
-cd Terninger.Console\bin\Release\netcoreapp3.0\publish
-"c:\Program Files\7-Zip\7z.exe" a -mx9 ..\..\..\..\..\Terninger.Console.netcoreapp30.zip *
+rem Build ZIP file for .NET 6.0
+del /q Terninger.Console.netcoreapp60.zip
+cd Terninger.Console\bin\Release\net60\publish
+"c:\Program Files\7-Zip\7z.exe" a -mx9 ..\..\..\..\..\Terninger.Console.net60.zip *
 cd ..\..\..\..\..
-"c:\Program Files\7-Zip\7z.exe" u Terninger.Console.netcoreapp30.zip LICENSE.txt
-
+"c:\Program Files\7-Zip\7z.exe" u Terninger.Console.net60.zip LICENSE.txt
+"c:\Program Files\7-Zip\7z.exe" d Terninger.Console.net60.zip Terninger.Console.exe.config
+"c:\Program Files\7-Zip\7z.exe" d Terninger.Console.net60.zip Terninger.Console.dll.config
 
 rem TOOD: generate hashes and signatures
 
